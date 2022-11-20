72e18f36b101627b6ebe7572078485450e7f7ecc
ÇETİN <cetinnnarasss@gmail.com>
2022-11-20 13:31:04 +0300

    authSlice

---
+++ src/features/authSlice.jsx
@@ -0,0 +1,18 @@
+import {createSlice } from '@reduxjs/toolkit';
+
+const initialState ={
+    user: '',
+};
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState:'initialState',
+    reducers:{
+        setUser:(state,action) =>{
+            state.user = action.payload;
+        },
+        clearUser: (state) => {
+            state.user = '';
+        }, 
+    },
+});
